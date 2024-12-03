35afd5e39f30c395e5dc18b25f7f2273d6188e78
happy-f <2200454726@qq.com>
2024-12-03 23:11:29 +0800

    ultimately

--- boost/Asio/timer/server.cpp
+++ boost/Asio/timer/server.cpp
@@ -8,37 +8,37 @@ void repeat(boost::asio::steady_timer& timer, boost::asio::ip::tcp::socket& sock
     boost::asio::async_write(socket, boost::asio::buffer(heartbeat_message), [&](const boost::system::error_code& ec, size_t) {
         if (!ec) {
             std::cout << "send heartbeat message" << std::endl;
+            // 只有在发送成功时才设置下一次定时器
+            timer.expires_at(timer.expiry() + boost::asio::chrono::seconds(3));
+            timer.async_wait([&](const boost::system::error_code& ec) {
+                if (!ec) {
+                    repeat(timer, socket);
+                }
+                });
         } else {
             std::cout << "Failed to send a heartbeat message" << std::endl;
             timer.cancel();
         }
         });
-        //end
-    timer.expires_at(timer.expiry() + boost::asio::chrono::seconds(3));
-    timer.async_wait([&](const boost::system::error_code& ec) {
-        if (!ec) {
-            repeat(timer, socket);
-        }
-        });
-
 }
+
 int main() {
     boost::asio::io_context io;
-    boost::asio::steady_timer timer(io, boost::asio::chrono::seconds(5));
-
-    boost::asio::ip::tcp::socket socket(io);
     boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), 12345);
     boost::asio::ip::tcp::acceptor acceptor(io, endpoint);
-    acceptor.async_accept([&](const boost::system::error_code& ec, boost::asio::ip::tcp::socket socket) {
+
+    // 将socket移到堆上，确保其生命周期
+    auto socket_ptr = std::make_shared<boost::asio::ip::tcp::socket>(io);
+    auto timer_ptr = std::make_shared<boost::asio::steady_timer>(io, boost::asio::chrono::seconds(5));
+
+    acceptor.async_accept(*socket_ptr, [socket_ptr, timer_ptr](const boost::system::error_code& ec) {
         if (!ec) {
             std::cout << "客户端已连接" << std::endl;
-            repeat(timer, socket);
-            // 处理客户端请求
+            repeat(*timer_ptr, *socket_ptr);
         } else {
             std::cerr << "连接失败: " << ec.message() << std::endl;
         }
         });
-
 
     io.run();
     return 0;
