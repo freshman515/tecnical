c54878cb463b3907f7bf9c296a5144f88f4f1280
happy-f <2200454726@qq.com>
2024-12-03 21:39:42 +0800

    motify heartbeat message

--- boost/Asio/timer/timerbase.cpp
+++ boost/Asio/timer/timerbase.cpp
@@ -1,16 +1,39 @@
 #include<iostream>
 #include<boost/asio.hpp>
 
+void repeat(boost::asio::steady_timer& timer, boost::asio::ip::tcp::socket& socket) {
+    //执行任务
+    std::cout << "开始执行任务" << std::endl;
+    const std::string heartbeat_message = "HEARTBEAD";
+    boost::asio::async_write(socket, boost::asio::buffer(heartbeat_message), [](const boost::system::error_code& ec, size_t) {
+        if (!ec) {
+            std::cout << "send heartbeat message" << std::endl;
+        }
+        });
+        //end
+    timer.expires_at(timer.expiry() + boost::asio::chrono::seconds(3));
+    timer.async_wait([&](const boost::system::error_code& ec) {
+        if (!ec) {
+            repeat(timer, socket);
+        }
+        });
+
+}
 int main() {
     boost::asio::io_context io;
     boost::asio::steady_timer timer(io, boost::asio::chrono::seconds(5));
-    timer.async_wait([](const boost::system::error_code& ec) {
+
+    boost::asio::ip::tcp::socket socket(io);
+    boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), 12345);
+    socket.async_connect(endpoint, [&](const boost::system::error_code& ec) {
         if (!ec) {
-            std::cout << "time is exit" << std::endl;
+            repeat(timer, socket);
+            std::cout << "connect successfully" << std::endl;
         } else {
-            std::cout << "timer async wait happen error" << std::endl;
+            std::cout << "connect fail" << std::endl;
         }
         });
+
 
     io.run();
     return 0;
